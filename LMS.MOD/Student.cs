5ba37cfea90b9c936faac6fca51757d9b5fc5918
arshadahamed <arshadpayoneer@gmail.com>
2025-02-03 15:35:20 +0530

    Feature : Added Student data access (DAL) and business logic (BL) layers

---
+++ LMS.MOD/Student.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace LMS.MOD
+{
+    public class Student
+    {
+        public int ID { get; set; }
+        public string StudentID { get; set; }
+        public string NameOfStudent { get; set; }
+        public DateTime DOB { get; set; }
+        public string Stream { get; set; }
+        public int AcademicYear { get; set; }
+        public string PermanentAddress { get; set; }
+        public string PresentAddress { get; set; }
+        public string Email { get; set; }
+        public string ParentsContact { get; set; }
+        public string StudentContact { get; set; }
+        public byte[] Photo { get; set; }
+    }
+}
