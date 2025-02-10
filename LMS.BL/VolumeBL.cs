15a4fddb4dbf0f74d1b94c9f845a4b916f875416
arshadahamed <arshadpayoneer@gmail.com>
2025-02-11 00:43:58 +0530

    FeatureANDModification-Volume and Journal Add, Manage, View with Volumes

---
+++ LMS.BL/VolumeBL.cs
@@ -0,0 +1,74 @@
+﻿using LMS.DAL;
+using LMS.MOD;
+using System.Collections.Generic;
+using System;
+
+namespace LMS.BL
+{
+    public class VolumeBL
+    {
+        public static Volume Save(Volume volume, int volumeID = -1)
+        {
+            try
+            {
+                volume = VolumeDAL.Save(volume, volumeID);
+                return volume;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        public static List<Volume> Get(int volumeID = -1, string title = "", int journalID = -1)
+        {
+            try
+            {
+                List<Volume> volumes = VolumeDAL.Get(volumeID, title, journalID);
+                return volumes;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        public static Volume GetByID(int volumeID)
+        {
+            try
+            {
+                Volume volume = VolumeDAL.GetByID(volumeID);
+                return volume;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        public static Volume Deactivate(Volume volume)
+        {
+            try
+            {
+                volume = VolumeDAL.Deactivate(volume);
+                return volume;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        public static List<Volume> GetByJournal(int journalID)
+        {
+            try
+            {
+                return VolumeDAL.GetByJournal(journalID);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Error fetching volumes by journal: " + ex.Message);
+            }
+        }
+    }
+}
